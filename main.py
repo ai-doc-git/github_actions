c55b81df3f1b7c3919871cb97302aa2ed17c9956
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-12 15:47:21 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-def sum_of_two_numbers(a, b):
-    return a + b
+def sum_of_two_numbers(first_num, second_num):
+    return first_num + second_num
 
-def numpy_sqrt_op(a):
-    return int(np.sqrt(a))
+def numpy_sqrt_op(num):
+    return int(np.sqrt(num))
 
 if __name__ == "__main__":
     print(sum_of_two_numbers(1,9))
