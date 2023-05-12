52068d3b2d5c7e83102c9a3f13faabbe9648870a
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-12 15:49:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import numpy as np
 
 def sum_of_two_numbers(first_num, second_num):
+    """
+    Function to add two numbers.
+    """
     return first_num + second_num
 
 def numpy_sqrt_op(num):
+    """
+    Function to calculate square root of a number.
+    """
     return int(np.sqrt(num))
 
 if __name__ == "__main__":
