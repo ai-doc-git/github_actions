f73ff6d93ed791e6d005990d74f91f6e430b47ce
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-14 16:29:11 +0530

    modified as per pylint errors

--- test_cases/test_cases.py
+++ test_cases/test_cases.py
@@ -10,13 +10,13 @@ def test_sum_function_01():
     Test Case 01
     """
     assert sum_of_two_numbers(15, 2) == 17
-    
+
 def test_sum_function_02():
     """
     Test Case 02
     """
     assert sum_of_two_numbers(0, 1) == 1
-    
+
 def test_sum_function_03():
     """
     Test Case 03
