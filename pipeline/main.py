51661260f8307a974dfd06bb5ccd3eb5ee75a1e1
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-15 10:07:49 +0530

    Mod_06

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,7 +1,7 @@
 """
 Main module.
 """
-from function.basic_ops import *
+from function.basic_ops import sum_of_two_numbers, numpy_sqrt_op
 
 if __name__ == "__main__":
     print(sum_of_two_numbers(1,9))
