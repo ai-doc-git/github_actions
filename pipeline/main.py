082a34cfbb29b896d45063732bb0e51eb548d221
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-15 09:56:45 +0530

    mod_03

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,20 +1,7 @@
 """
 Main module.
 """
-
-import numpy as np
-
-def sum_of_two_numbers(first_num, second_num):
-    """
-    Function to add two numbers.
-    """
-    return first_num + second_num
-
-def numpy_sqrt_op(num):
-    """
-    Function to calculate square root of a number.
-    """
-    return int(np.sqrt(num))
+from function.basic_ops import *
 
 if __name__ == "__main__":
     print(sum_of_two_numbers(1,9))
