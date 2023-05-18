1051256d5f01643ff52e278b1bc03950ed7116fd
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-18 16:52:08 +0530

    Update basic_ops.py

--- pipeline/function/basic_ops.py
+++ pipeline/function/basic_ops.py
@@ -14,3 +14,12 @@ def numpy_sqrt_op(num):
     Function to calculate square root of a number.
     """
     return int(np.sqrt(num))
+
+def append_create(num):
+    """
+    Function to calculate square root of a number.
+    """
+    lst = []
+    for item in range(0,10):
+        lst = lst.append(item)
+    return lst
