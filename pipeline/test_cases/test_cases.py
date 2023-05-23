e9258f24f0feb5bd98a6f30ceea4a9c69461acc7
NIKHIL RAJ <nkr4nikhilraj@gmail.com>
2023-05-23 11:47:58 +0530

    Added test cases for append_list function

--- pipeline/test_cases/test_cases.py
+++ pipeline/test_cases/test_cases.py
@@ -4,7 +4,7 @@ Test Cases module.
 
 from os import sys, path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
-from function.basic_ops import sum_of_two_numbers
+from function.basic_ops import sum_of_two_numbers, append_list
 
 def test_sum_function_01():
     """
@@ -23,3 +23,15 @@ def test_sum_function_03():
     Test Case 03
     """
     assert sum_of_two_numbers(4, 4) == 8
+
+def test_append_list_function_01():
+    """
+    Test Case 04
+    """
+    assert append_list(5) == [0,1,2,3,4]
+    
+def test_append_list_function_02():
+    """
+    Test Case 05
+    """
+    assert append_list(10) == [0,1,2,3,4,5,6,7,8,9]
